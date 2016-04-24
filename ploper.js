5c1617bd1324a0b654f85c94e39e7582115f936b
Yuan Tao <towyuan@outlook.com>
2016-04-24 17:16:49 +0800

    Added output, folder and custom options to plop for plopfile

--- ploper.js
+++ ploper.js
@@ -9,17 +9,23 @@ var inquirer = require('inquirer');
  * Load plopfile, enhance plop and return the plop instance.
  *
  * @param {String} plopfile The plopfile path.
- * @param {String} [target] The target directory where files generated. Default is same with plopfile.
+ * @param {String|Object} [output] The output directory where files generated. Default is same with plopfile.
+ * @param {Object} [options]
  * @returns {*}
  */
-exports.plop = function (plopfile, target) {
+exports.plop = function (plopfile, output, options) {
   var logic = require('plop/mod/logic');
   var plop = require('plop/mod/plop-base');
 
-  target = target || path.dirname(plopfile);
+  if (typeof output !== 'string') {
+    options = output;
+    output = null;
+  }
+  options = options || {};
+  output = output || options.output || path.dirname(plopfile);
 
-  if (!fs.existsSync(target)) {
-    fs.mkdirpSync(target);
+  if (!fs.existsSync(output)) {
+    fs.mkdirpSync(output);
   }
 
   Object.defineProperty(plop, 'generators', {
@@ -29,13 +35,13 @@ exports.plop = function (plopfile, target) {
   });
 
   /**
-   * Resolve output file from target
+   * Resolve output file from output
    *
    * @param file
    * @returns {string}
      */
   plop.resolve = function (file) {
-    return path.resolve(target, file);
+    return path.resolve(output, file);
   };
 
   /**
@@ -85,6 +91,9 @@ exports.plop = function (plopfile, target) {
     });
   };
 
+  plop.output = output;
+  plop.folder = path.basename(output);
+  plop.options = options;
   plop.setPlopfilePath(path.dirname(plopfile));
 
   // load plopfile
